14e6d937bf9d8fff137d08456956ee2a5554dee0
Sophie The Lionhart <sophie-lionhart@idexx.com>
2024-08-06 19:00:59 -0500

    changed non-table layout

--- assets/scripts/src/components/frequentlyAskedQuestions.ts
+++ assets/scripts/src/components/frequentlyAskedQuestions.ts
@@ -3,35 +3,59 @@ import CustomElement from "../../lib/customElement";
 export default class FrequentlyAskedQuestions extends CustomElement {
     protected readonly STYLE = `
         <style>
+            article {
+                width: var(--view-amount);
+                margin: 1em auto;
+                padding: 1em;
+                border-radius: .5em;
+                border: solid black .10em;
+                box-shadow: .5em .5em .5em rgba(1, 1, 1, .4);
+            }
+            
+            details {
+                border: solid black .1em;
+                margin: 1em;
+            }
+        
             details summary h3 {
                 display: inline;
+            }
+            
+            details:nth-child(even) {
+                background-color: rgba(1, 1, 1, 0.2);
+            }
+            
+            details:nth-child(odd) {
+                background-color: white;
             }
         </style>
     `;
 
     protected readonly TEMPLATE = `
-        <h2>Frequently Asked Questions</h2>
-        <details>
-            <summary><h3>Why Should We Care About This</h3></summary>
-            <p>
-                Hate Bad. Duh!
-            </p>
-        </details>
-        <details>
-            <summary><h3>Are You Guys Political</h3></summary>
-            <p>
-                No. We believe that hate and intolerance is not something unique to any side of the political aisle 
-                and should be fought at all opportunities by everybody on all sides.
-            </p>
-        </details>
-        <details open>
-            <summary><h3>What is your agenda?</h3></summary>
-            <p>
-                As two individuals from marginalized communities we grew up being victims of hate and intolerance. 
-                Anyone who grew up underprivileged in any aspects of their life or were bullied for any reason understand
-                what it is like to be the victim of the abuse that comes from bigotry. We hope to eliminate that type 
-                of suffering from this world. :) 
-            </p>
-        </details>
+        <article>
+            <h2>Frequently Asked Questions</h2>
+            <details>
+                <summary><h3>Why Should We Care About This</h3></summary>
+                <p>
+                    Hate Bad. Duh!
+                </p>
+            </details>
+            <details>
+                <summary><h3>Are You Guys Political</h3></summary>
+                <p>
+                    No. We believe that hate and intolerance is not something unique to any side of the political aisle 
+                    and should be fought at all opportunities by everybody on all sides.
+                </p>
+            </details>
+            <details open>
+                <summary><h3>What is your agenda?</h3></summary>
+                <p>
+                    As two individuals from marginalized communities we grew up being victims of hate and intolerance. 
+                    Anyone who grew up underprivileged in any aspects of their life or were bullied for any reason understand
+                    what it is like to be the victim of the abuse that comes from bigotry. We hope to eliminate that type 
+                    of suffering from this world. :) 
+                </p>
+            </details>
+        </article>
     `;
 }
--- assets/scripts/src/components/getInvolved.ts
+++ assets/scripts/src/components/getInvolved.ts
@@ -1,29 +1,62 @@
 import CustomElement from "../../lib/customElement";
 
 export default class GetInvolved extends CustomElement {
-    protected readonly STYLE = `<style></style>`;
+    protected readonly STYLE = `<style>
+        article {
+            width: var(--view-amount);
+            margin: 1em auto;
+            padding: 1em;
+            border-radius: .5em;
+            border: solid black .10em;
+            box-shadow: .5em .5em .5em rgba(1, 1, 1, .4);
+        }
+        
+        article > div {
+            display: flex;
+            justify-content: space-around;
+        }
+        
+        section {
+            width: 40%;
+            margin: 1em 0;
+            padding: 1em;
+            border-radius: .5em;
+            border: solid black .10em;
+            box-shadow: .5em .5em .5em rgba(1, 1, 1, .4);
+        }
+    </style>`;
 
     protected readonly TEMPLATE = `
-        <h2>Get Involved</h2>
-        <p>
-            Hate and Intolerance will never pause in its efforts to spread and live on. It is up to us to remain 
-            vigilant and fight it in all its forms whenever we see it and can do so safely. Are you ready to do more to 
-            eliminate hatred in this world? 
-        </p>
-        <h3>In the day-to-day</h3>
-        <ul>
-            <li>The best way to fight hatred and intolerance in the world is to call it out when you see it.</li>
-            <li>Go through this site with friends and family and people in your life you care about.</li>
-            <li>Share this site on social media!</li>
-            <li>Contribute to the organizations found in the elements</li>
-            <li>Help those around you with love and compassion!</li>
-        </ul>
-        <h3>With the Organization</h3>
-        <ul>
-            <li>Join our Discord to discuss ways to educate your community and meet others who want the same in your area!</li>
-            <li>Join us on GitHub and contribute to the website and work on our roadmap!</li>
-            <li>Donate! We'd love to hire translators or be able to work on this full time!</li>
-            <li>Do research and compile information to be input into the elements!</li>
-        </ul>
+        <article>
+            <header>
+                <h2>Get Involved</h2>
+                <p>
+                    Hate and Intolerance will never pause in its efforts to spread and live on. It is up to us to remain 
+                    vigilant and fight it in all its forms whenever we see it and can do so safely. Are you ready to do more to 
+                    eliminate hatred in this world? 
+                </p>
+            </header>
+            <div>
+                <section>
+                    <h3>In the day-to-day</h3>
+                    <ul>
+                        <li>The best way to fight hatred and intolerance in the world is to call it out when you see it.</li>
+                        <li>Go through this site with friends and family and people in your life you care about.</li>
+                        <li>Share this site on social media!</li>
+                        <li>Contribute to the organizations found in the elements</li>
+                        <li>Help those around you with love and compassion!</li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>With the Organization</h3>
+                    <ul>
+                        <li>Join our Discord to discuss ways to educate your community and meet others who want the same in your area!</li>
+                        <li>Join us on GitHub and contribute to the website and work on our roadmap!</li>
+                        <li>Donate! We'd love to hire translators or be able to work on this full time!</li>
+                        <li>Do research and compile information to be input into the elements!</li>
+                    </ul>
+                </section>
+            </div>
+        </article>
     `;
 }
